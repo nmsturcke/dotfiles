610c0004541a5befcac9565e63667054dae5295c
Nickyux <30734036+nmsturcke@users.noreply.github.com>
2025-03-27 18:37:27 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -25,6 +25,8 @@ local ensure_packer = function()
     
     -- LSP Configuration
     use 'neovim/nvim-lspconfig'
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
     
     -- Completion plugins
     use 'hrsh7th/nvim-cmp'
@@ -76,6 +78,10 @@ local ensure_packer = function()
     -- ChatGPT again!
     use {'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons'}
   
+    -- Prettier
+    use('neovim/nvim-lspconfig')
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
   
     -- Multi Cursor, from https://github.com/jake-stewart/multicursor.nvim 
     -- use {
@@ -147,6 +153,11 @@ local ensure_packer = function()
   vim.opt.smartindent = true
   vim.opt.termguicolors = true
   
+  require('mason').setup()
+  require('mason-lspconfig').setup({
+      ensure_installed = { 'pyright', 'pylsp' }
+  })
+
   -- Set up LSP
   local lspconfig = require('lspconfig')
   
@@ -164,9 +175,92 @@ local ensure_packer = function()
     }
   })
   
+  -- Javascript
+  local null_ls = require("null-ls")
+
+  local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+  local event = "BufWritePre" -- or "BufWritePost"
+  local async = event == "BufWritePost"
   
+  local prettier = require("prettier")
+
+  prettier.setup({
+    bin = 'prettierd',
+    filetypes = {
+      "css",
+      "graphql",
+      "html",
+      "javascript",
+      "javascriptreact",
+      "json",
+      "less",
+      "markdown",
+      "scss",
+      "typescript",
+      "typescriptreact",
+      "yaml",
+    },
+    cli_options = {
+      arrow_parens = "always",
+      bracket_spacing = true,
+      bracket_same_line = false,
+      embedded_language_formatting = "auto",
+      end_of_line = "lf",
+      html_whitespace_sensitivity = "css",
+      -- jsx_bracket_same_line = false,
+      jsx_single_quote = false,
+      print_width = 80,
+      prose_wrap = "preserve",
+      quote_props = "as-needed",
+      semi = true,
+      single_attribute_per_line = false,
+      single_quote = false,
+      tab_width = 4,
+      trailing_comma = "es5",
+      use_tabs = false,
+    },
+
+  })
+
+  null_ls.setup({
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.keymap.set("n", "<Leader>f", function()
+          vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        end, { buffer = bufnr, desc = "[lsp] format" })
+  
+        -- format on save
+        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+        vim.api.nvim_create_autocmd(event, {
+          buffer = bufnr,
+          group = group,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr, async = async })
+          end,
+          desc = "[lsp] format on save",
+        })
+      end
+  
+      if client.supports_method("textDocument/rangeFormatting") then
+        vim.keymap.set("x", "<Leader>f", function()
+          vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        end, { buffer = bufnr, desc = "[lsp] format" })
+      end
+    end,
+  })
   -- Configure rust-analyzer
   require('rust-tools').setup({
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
     server = {
       settings = {
         ["rust-analyzer"] = {
@@ -214,7 +308,7 @@ local ensure_packer = function()
   -- Set up NvimTree
   require('nvim-tree').setup({
     view = {
-      width = 30,
+      width = 20,
     },
     renderer = {
       icons = {
@@ -246,7 +340,7 @@ local ensure_packer = function()
   
   -- Set up lualine
   require('lualine').setup()
-  
+
   -- Format on save for Rust files
   vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.rs",
@@ -288,3 +382,4 @@ local ensure_packer = function()
   vim.cmd [[
     command! PyrightPath lua print(vim.inspect(require('lspconfig').pyright.settings))
   ]]
+  
