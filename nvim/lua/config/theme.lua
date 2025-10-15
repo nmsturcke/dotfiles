6d6079977a39959693df5231438dc80024a30f08
nmsturcke <me@nickyux.com>
2025-10-15 16:11:40 +0200

    feat: more readable line numbers, +Theme command

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,9 +1,31 @@
+vim.api.nvim_create_user_command("Theme", function (opts)
+    vim.cmd("colorscheme " .. opts.args)
+
+    local bg = vim.o.background -- this will be "dark" or "light"
+
+    if bg == "dark" then
+        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#B3B3B3", bold=false })
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#D3D3D3", bold=true })
+        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#B3B3B3", bold=false })
+    else
+        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#4D4D4D", bold=false })
+        vim.api.nvim_set_hl(0, "LineNr", { fg = "#262626", bold=true })
+        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#4D4D4D", bold=false })
+    end
+end, {
+    nargs = 1,
+    complete = function(ArgLead, _, _)
+        return vim.fn.getcompletion(ArgLead, "color")
+    end,
+})
+
+
 if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0.150
     vim.g.neovide_cursor_trail_size = 0.9
     vim.g.neovide_normal_opacity = 1 
     vim.g.neovide_cursor_vfx_mode= "pixiedust"
-    vim.cmd("colorscheme ayu-mirage")
+    vim.cmd("Theme ayu-mirage")
 
     vim.api.nvim_create_user_command("Opacity", function (opts)
         local args = opts.args
@@ -32,5 +54,5 @@ else
         },
     })
 
-    vim.cmd("colorscheme ayu-mirage")
+    vim.cmd("Theme ayu-mirage")
 end
